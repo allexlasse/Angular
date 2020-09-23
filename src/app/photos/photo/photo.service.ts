44fecb9b1bb28115a1fda6c752e712d6a90f2a39
Allex Lasse <allex_lasse@hotmail.com>
2020-09-23 16:22:07 -0300

    modulo 3 completo

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -4,8 +4,9 @@ import { Photo } from "./photo";
 import { PhotoComment } from '../photo-detail/photo-comments/photo-comment';
 import { catchError, map } from 'rxjs/operators';
 import { of, throwError } from 'rxjs';
+import {environment} from '../../../environments/environment';
 
-const API = 'http://localhost:3000';
+const API_URL = environment.ApiUrl;
 
 @Injectable({providedIn: 'root'})
 export class PhotoService {
@@ -15,7 +16,7 @@ export class PhotoService {
     
     listFromUser(userName: string){
 
-        return this.http.get<Photo[]>(API + `/${userName}/photos`);
+        return this.http.get<Photo[]>(API_URL + `/${userName}/photos`);
 
     }
 
@@ -23,7 +24,7 @@ export class PhotoService {
 
         const params = new HttpParams().append('page', page.toString());
 
-        return this.http.get<Photo[]>(API + `/${userName}/photos`, {params});
+        return this.http.get<Photo[]>(API_URL + `/${userName}/photos`, {params});
 
     }
 
@@ -33,36 +34,36 @@ export class PhotoService {
         formData.append('description', description)
         formData.append('allowComments', allowComments ? 'true' : 'false');
         formData.append('imageFile', file)
-        return this.http.post(API + `/photos/upload`, formData); 
+        return this.http.post(API_URL + `/photos/upload`, formData); 
     }
 
     fingById(photoId: number){
-        return this.http.get<Photo>(`${API}/photos/${photoId}`);
+        return this.http.get<Photo>(`${API_URL}/photos/${photoId}`);
     }
 
     getComments(photoId: number){
-        return this.http.get<PhotoComment[]>(`${API}/photos/${photoId}/comments`);
+        return this.http.get<PhotoComment[]>(`${API_URL}/photos/${photoId}/comments`);
     }
 
     addComment(photoId: number, commentText: string){
-        return this.http.post<PhotoComment[]>(`${API}/photos/${photoId}/comments`, {commentText});
+        return this.http.post<PhotoComment[]>(`${API_URL}/photos/${photoId}/comments`, {commentText});
     }
 
     removePhoto(photoId: number){
-        return this.http.delete(`${API}/photos/${photoId}`);
+        return this.http.delete(`${API_URL}/photos/${photoId}`);
     }
 
     like(photoId: number){
 
         return this.http.post(
-            API + '/photos/' + photoId +  '/like', {}, {observe: 'response'}
+            API_URL + '/photos/' + photoId +  '/like', {}, {observe: 'response'}
         )
         .pipe(map(res => true))
         .pipe(catchError(err => {
             return err.status == '304' ? of(false) : throwError(err);
         }));
 
-        // return this.http.post(`${API}/photos/${photoId}/like`, {}, {observe: 'response'})
+        // return this.http.post(`${API_URL}/photos/${photoId}/like`, {}, {observe: 'response'})
         // .pipe(map(res => true))
         // .pipe(catchError(err => {
         //     return err.status == 304 ? of(false): throwError(err);
